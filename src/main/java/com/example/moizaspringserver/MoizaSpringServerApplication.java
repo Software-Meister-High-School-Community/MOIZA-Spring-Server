d5570f8cb7a6eb5dc866e696704e8a3e979217c6
lyutvs <lyutvs@gmail.com>
2022-03-01 22:49:33 +0900

    ⚙️ ::  프로젝트 생성

---
+++ src/main/java/com/example/moizaspringserver/MoizaSpringServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.moizaspringserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoizaSpringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoizaSpringServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moizaspringserver/MoizaSpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moizaspringserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoizaSpringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
