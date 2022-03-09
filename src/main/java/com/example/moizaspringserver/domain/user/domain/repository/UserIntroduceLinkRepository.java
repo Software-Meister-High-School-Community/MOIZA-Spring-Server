65d5c0f089234f215d31f0ec7d302613674d44f8
lyutvs <lyutvs@gmail.com>
2022-03-09 19:53:36 +0900

    📑:: UserIntroduceLinkReposiroy

---
+++ src/main/java/com/example/moizaspringserver/domain/user/domain/repository/UserIntroduceLinkRepository.java
@@ -0,0 +1,7 @@
+package com.example.moizaspringserver.domain.user.domain.repository;
+
+import com.example.moizaspringserver.domain.user.domain.UserIntroduceLink;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserIntroduceLinkRepository extends CrudRepository<UserIntroduceLink, Long>{
+}
