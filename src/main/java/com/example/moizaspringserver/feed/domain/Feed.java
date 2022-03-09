8c70ea7b525c3b5952df18a8131b8969006e50e2
lyutvs <lyutvs@gmail.com>
2022-03-09 16:47:09 +0900

    ⚡️:: BasTimeIdEntity 추가

--- src/main/java/com/example/moizaspringserver/feed/domain/Feed.java
+++ src/main/java/com/example/moizaspringserver/feed/domain/Feed.java
@@ -2,6 +2,7 @@ package com.example.moizaspringserver.feed.domain;
 
 import com.example.moizaspringserver.domain.user.domain.User;
 import com.example.moizaspringserver.feed.domain.type.FeedType;
+import com.example.moizaspringserver.global.entity.BaseTimeIdEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,7 +15,7 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "tbl_feed")
-public class Feed {
+public class Feed extends BaseTimeIdEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
