545c30e36d7b774c6f0e1bad2478481eb3490d24
VINTO1819 <develretr0@gmail.com>
2022-03-10 20:33:41 +0900

    ⚡️:: 페이징 지원을 위해 JpaRepository 기반으로 변경

--- src/main/java/com/example/moizaspringserver/domain/notice/domain/repository/NoticeRepository.java
+++ src/main/java/com/example/moizaspringserver/domain/notice/domain/repository/NoticeRepository.java
@@ -1,7 +1,7 @@
 package com.example.moizaspringserver.domain.notice.domain.repository;
 
 import com.example.moizaspringserver.domain.notice.domain.Notice;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NoticeRepository extends CrudRepository<Notice, Long> {
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
 }
