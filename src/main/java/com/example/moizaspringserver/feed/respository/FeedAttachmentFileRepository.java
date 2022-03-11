61fd287aadc9b6d4a73a4ffff6bc2da394c5b198
lyutvs <lyutvs@gmail.com>
2022-03-11 21:29:58 +0900

    📑 :: FeedAttachmentFileRepository

---
+++ src/main/java/com/example/moizaspringserver/feed/respository/FeedAttachmentFileRepository.java
@@ -0,0 +1,7 @@
+package com.example.moizaspringserver.feed.respository;
+
+import com.example.moizaspringserver.feed.entity.FeedAttachmentFile;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FeedAttachmentFileRepository extends CrudRepository<FeedAttachmentFile, Long> {
+}
