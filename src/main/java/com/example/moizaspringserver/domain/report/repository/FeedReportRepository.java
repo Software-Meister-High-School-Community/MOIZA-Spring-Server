dd9fe92b5cda7e01c38c4fcd2bccc3413686af98
김범진 <sdpthf@naver.com>
2022-03-13 00:15:51 +0900

    ⚡ :: jpa repository로 변경

--- src/main/java/com/example/moizaspringserver/domain/report/repository/FeedReportRepository.java
+++ src/main/java/com/example/moizaspringserver/domain/report/repository/FeedReportRepository.java
@@ -2,7 +2,7 @@ package com.example.moizaspringserver.domain.report.repository;
 
 import com.example.moizaspringserver.domain.report.entity.FeedReport;
 import com.example.moizaspringserver.domain.report.entity.FeedReportId;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FeedReportRepository extends CrudRepository<FeedReport, FeedReportId> {
+public interface FeedReportRepository extends JpaRepository<FeedReport, FeedReportId> {
 }
--- src/main/java/com/example/moizaspringserver/domain/report/repository/UserReportRepository.java
+++ src/main/java/com/example/moizaspringserver/domain/report/repository/UserReportRepository.java
@@ -2,7 +2,7 @@ package com.example.moizaspringserver.domain.report.repository;
 
 import com.example.moizaspringserver.domain.report.entity.UserReport;
 import com.example.moizaspringserver.domain.report.entity.UserReportId;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserReportRepository extends CrudRepository<UserReport, UserReportId> {
+public interface UserReportRepository extends JpaRepository<UserReport, UserReportId> {
 }
