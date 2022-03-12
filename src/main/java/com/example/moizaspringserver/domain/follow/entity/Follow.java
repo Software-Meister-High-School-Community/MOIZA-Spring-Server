2c8a43cefaea5545b7f4032a4104667d763a28b5
김범진 <sdpthf@naver.com>
2022-03-12 23:58:11 +0900

    ⚡ :: FollowId 적용

--- src/main/java/com/example/moizaspringserver/domain/follow/entity/Follow.java
+++ src/main/java/com/example/moizaspringserver/domain/follow/entity/Follow.java
@@ -1,35 +1,21 @@
 package com.example.moizaspringserver.domain.follow.entity;
 
-import com.example.moizaspringserver.domain.user.entity.User;
-import com.example.moizaspringserver.global.entity.BaseTimeIdEntity;
+import com.example.moizaspringserver.global.entity.BaseTimeEntity;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "tbl_follow")
-public class Follow extends BaseTimeIdEntity {
+public class Follow extends BaseTimeEntity {
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    @EmbeddedId
+    private FollowId id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "target_user_id", nullable = false)
-    private User targetUser;
-
-    @Builder
-    public Follow(User user, User targetUser) {
-        this.user = user;
-        this.targetUser = targetUser;
-    }
 }
