5b1df6649c672c3255239a8373000ff28f81c46f
lyutvs <lyutvs@gmail.com>
2022-03-12 22:55:10 +0900

    📑 :: PublicFeed

---
+++ src/main/java/com/example/moizaspringserver/feed/entity/PublicFeed.java
@@ -0,0 +1,49 @@
+package com.example.moizaspringserver.feed.entity;
+
+import com.example.moizaspringserver.global.entity.BaseIdEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@DynamicInsert
+@Table(name = "tbl_public_feed")
+public class PublicFeed extends BaseIdEntity {
+    @NotNull
+    @Length(max = 30)
+    private String title;
+
+    @NotNull
+    @Length(max = 500)
+    private String content;
+
+    @NotNull
+    @ColumnDefault("0")
+    private LocalDateTime updatedAt;
+
+    @NotNull
+    private Integer likeCount;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id", nullable = false)
+    private Feed feed;
+
+    @Builder
+    public PublicFeed(String title, String content, LocalDateTime updatedAt, Integer likeCount, Feed feed) {
+        this.title = title;
+        this.content = content;
+        this.updatedAt = updatedAt;
+        this.likeCount = likeCount;
+        this.feed = feed;
+    }
+}
