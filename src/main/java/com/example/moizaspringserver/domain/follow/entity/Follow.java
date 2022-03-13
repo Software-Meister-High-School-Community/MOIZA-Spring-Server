e3e36c7e0ec0c58eb758f034a9671dd06405b6b4
김범진 <sdpthf@naver.com>
2022-03-13 15:34:36 +0900

    ⚡ :: MapsId로 변경

--- src/main/java/com/example/moizaspringserver/domain/follow/entity/Follow.java
+++ src/main/java/com/example/moizaspringserver/domain/follow/entity/Follow.java
@@ -1,12 +1,18 @@
 package com.example.moizaspringserver.domain.follow.entity;
 
+import com.example.moizaspringserver.domain.user.entity.User;
 import com.example.moizaspringserver.global.entity.BaseTimeEntity;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
 import javax.persistence.Table;
 
 @Getter
@@ -18,4 +24,20 @@ public class Follow extends BaseTimeEntity {
     @EmbeddedId
     private FollowId id;
 
+    @MapsId
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @MapsId
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "target_user_id", nullable = false)
+    private User targetUser;
+
+    @Builder
+    public Follow(FollowId id, User user, User targetUser) {
+        this.id = id;
+        this.user = user;
+        this.targetUser = targetUser;
+    }
 }
--- src/main/java/com/example/moizaspringserver/domain/follow/entity/FollowId.java
+++ src/main/java/com/example/moizaspringserver/domain/follow/entity/FollowId.java
@@ -1,15 +1,11 @@
 package com.example.moizaspringserver.domain.follow.entity;
 
-import com.example.moizaspringserver.domain.user.entity.User;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import java.io.Serializable;
 
 @Getter
@@ -17,19 +13,14 @@ import java.io.Serializable;
 @Embeddable
 public class FollowId implements Serializable {
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    private Integer user;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "target_user_id", nullable = false)
-    private User targetUser;
+    private Integer targetUser;
 
     @Builder
-    public FollowId(User user, User targetUser) {
+    public FollowId(Integer user, Integer targetUser) {
         this.user = user;
         this.targetUser = targetUser;
     }
-
 }
 
