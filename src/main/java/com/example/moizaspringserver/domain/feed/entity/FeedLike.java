c2a65dda8583bc2a3deffaa17a8f0d02c77b4958
이준서 <ljchan71@gmail.com>
2022-03-13 22:41:11 +0900

    ⚡ :: MapsId 방식 변경

--- src/main/java/com/example/moizaspringserver/domain/feed/entity/FeedLike.java
+++ src/main/java/com/example/moizaspringserver/domain/feed/entity/FeedLike.java
@@ -1,8 +1,13 @@
 package com.example.moizaspringserver.domain.feed.entity;
 
+import com.example.moizaspringserver.domain.user.entity.User;
 import com.example.moizaspringserver.global.entity.BaseTimeEntity;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
 import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -18,8 +23,21 @@ public class FeedLike extends BaseTimeEntity {
 	@EmbeddedId
 	private FeedLikeId id;
 
+	@MapsId("user")
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@MapsId("feed")
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "feed_id", nullable = false)
+	private Feed feed;
+
 	@Builder
-	public FeedLike(FeedLikeId id) {
+	public FeedLike(FeedLikeId id, User user,
+		Feed feed) {
 		this.id = id;
+		this.user = user;
+		this.feed = feed;
 	}
 }
--- src/main/java/com/example/moizaspringserver/domain/feed/entity/FeedLikeId.java
+++ src/main/java/com/example/moizaspringserver/domain/feed/entity/FeedLikeId.java
@@ -1,11 +1,7 @@
 package com.example.moizaspringserver.domain.feed.entity;
 
-import com.example.moizaspringserver.domain.user.entity.User;
 import java.io.Serializable;
 import javax.persistence.Embeddable;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,16 +12,12 @@ import lombok.NoArgsConstructor;
 @Embeddable
 public class FeedLikeId implements Serializable {
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_id", nullable = false)
-	private User user;
+	private Integer user;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "feed_id", nullable = false)
-	private Feed feed;
+	private Integer feed;
 
 	@Builder
-	public FeedLikeId(User user, Feed feed) {
+	public FeedLikeId(Integer user, Integer feed) {
 		this.user = user;
 		this.feed = feed;
 	}
