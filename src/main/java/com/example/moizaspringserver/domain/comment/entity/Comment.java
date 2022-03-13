d93cffbb0e2db7c51affca10ffb0069561a7e153
VINTO1819 <develretr0@gmail.com>
2022-03-13 13:33:57 +0900

    📑 :: Comment 엔티티

---
+++ src/main/java/com/example/moizaspringserver/domain/comment/entity/Comment.java
@@ -0,0 +1,54 @@
+package com.example.moizaspringserver.domain.comment.entity;
+
+import com.example.moizaspringserver.domain.feed.entity.Feed;
+import com.example.moizaspringserver.domain.user.entity.User;
+import com.example.moizaspringserver.global.entity.BaseTimeIdEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@DynamicInsert
+@Table(name = "tbl_comment")
+public class Comment extends BaseTimeIdEntity {
+
+    @NotNull
+    @Length(max = 500)
+    private String content;
+
+    @NotNull
+    @ColumnDefault(value = "false")
+    private Boolean isPinned;
+
+    private LocalDateTime updated_at;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id", nullable = false)
+    private Feed feed;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_comment_id", referencedColumnName = "comment_id", nullable = false)
+    private Comment parentComment;
+
+    @Builder
+    public Comment(String content, User user, Feed feed, Comment parentComment) {
+        this.content = content;
+        this.user = user;
+        this.feed = feed;
+        this.parentComment = parentComment;
+    }
+}
