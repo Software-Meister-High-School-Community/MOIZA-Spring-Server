2b0caed49bc908f2e3d96293d806234dddadc5cf
이준서 <ljchan71@gmail.com>
2022-03-13 12:17:41 +0900

    📑 :: FeedCategory entity

---
+++ src/main/java/com/example/moizaspringserver/domain/feed/entity/FeedCategory.java
@@ -0,0 +1,18 @@
+package com.example.moizaspringserver.domain.feed.entity;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "tbl_feed_category")
+public class FeedCategory {
+
+	@EmbeddedId
+	private FeedCategoryId id;
+}
