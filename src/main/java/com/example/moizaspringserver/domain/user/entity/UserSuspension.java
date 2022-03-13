f9e25c173742595db34aa4e9ef4f42e038b3ae85
lyutvs <lyutvs@gmail.com>
2022-03-13 18:53:05 +0900

    📑 :: UserSuspension

---
+++ src/main/java/com/example/moizaspringserver/domain/user/entity/UserSuspension.java
@@ -0,0 +1,31 @@
+package com.example.moizaspringserver.domain.user.entity;
+
+import com.example.moizaspringserver.global.entity.BaseIdEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "tbl_user_suspension")
+public class UserSuspension extends BaseIdEntity {
+
+    @NotNull
+    private LocalDateTime expiredAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder
+    public UserSuspension(LocalDateTime expiredAt, User user) {
+        this.expiredAt = expiredAt;
+        this.user = user;
+    }
+}
