788fb5bc7d362c3e876f91a60d07a7a7fbc68a40
lyutvs <lyutvs@gmail.com>
2022-03-15 19:45:09 +0900

    📑 :: CustomException

---
+++ src/main/java/com/example/moizaspringserver/global/error/exception/CustomException.java
@@ -0,0 +1,12 @@
+package com.example.moizaspringserver.global.error.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomException extends RuntimeException{
+
+    private final ErrorCode errorCode;
+
+}
