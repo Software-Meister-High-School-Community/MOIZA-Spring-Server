69d75920c3321500d7e95a424ebf09b2716f50f3
VINTO1819 <develretr0@gmail.com>
2022-03-24 15:10:41 +0900

    📑 :: JwtTokenProvider 구현

---
+++ src/main/java/com/example/moizaspringserver/global/security/JwtTokenProvider.java
@@ -0,0 +1,48 @@
+package com.example.moizaspringserver.global.security;
+
+import com.example.moizaspringserver.global.config.JwtProperties;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    private final String SECRET_JWT;
+    public JwtTokenProvider(JwtProperties jwtProperties) {
+        SECRET_JWT = jwtProperties.getSecret();
+    }
+
+    public String generate(Authentication auth) {
+        Date now = new Date();
+        Date expiresAt = new Date();
+
+        return Jwts.builder()
+                .setSubject((String) auth.getPrincipal())
+                .setIssuedAt(now)
+                .setExpiration(expiresAt)
+                .signWith(SignatureAlgorithm.HS256, SECRET_JWT)
+                .compact();
+    }
+
+    public String getUserId(String jwtToken) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(SECRET_JWT)
+                .parseClaimsJws(jwtToken)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public Boolean validation(String jwtToken) {
+        try {
+            Jwts.parser().setSigningKey(SECRET_JWT).parseClaimsJws(jwtToken);
+            return true;
+        } catch (Exception ex) { return false; }
+    }
+
+}
