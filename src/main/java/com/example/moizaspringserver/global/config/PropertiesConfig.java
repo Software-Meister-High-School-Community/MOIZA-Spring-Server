234d86432073b8493fb8f06708c150e962edc87e
lyutvs <lyutvs@gmail.com>
2022-04-27 14:18:37 +0900

    📑 :: @ConfigurationPropertiesScan

---
+++ src/main/java/com/example/moizaspringserver/global/config/PropertiesConfig.java
@@ -0,0 +1,8 @@
+package com.example.moizaspringserver.global.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationPropertiesScan
+@Configuration
+public class PropertiesConfig { }
