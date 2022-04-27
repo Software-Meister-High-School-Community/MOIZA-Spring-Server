1c3c64967fdf497d39f504a4d4bd427322b1f44b
lyutvs <lyutvs@gmail.com>
2022-04-27 14:18:23 +0900

    ⚡️ :: UserRepository

--- src/main/java/com/example/moizaspringserver/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/moizaspringserver/domain/user/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.example.moizaspringserver.domain.user.repository;
 import com.example.moizaspringserver.domain.user.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+    Optional<User> findByAccountId(String accountId);
 }
