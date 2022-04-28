7cfcf6e1b4abf4425f6bba764f354964d6ce9d7b
lyutvs <lyutvs@gmail.com>
2022-04-28 11:55:22 +0900

    ⚡️ :: user.getAccountid()

--- src/main/java/com/example/moizaspringserver/global/security/auth/AuthDetails.java
+++ src/main/java/com/example/moizaspringserver/global/security/auth/AuthDetails.java
@@ -31,26 +31,26 @@ public class AuthDetails implements UserDetails {
 
     @Override
     public String getUsername() {
-        return null;
+        return user.getAccountId();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
