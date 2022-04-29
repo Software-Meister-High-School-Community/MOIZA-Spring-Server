d3916bdcd0f1727de35651cb8ee7251acad9cd7b
lyutvs <lyutvs@gmail.com>
2022-04-29 23:02:19 +0900

    ⚡️ :: AuthController

--- src/main/java/com/example/moizaspringserver/domain/auth/presentation/AuthController.java
+++ src/main/java/com/example/moizaspringserver/domain/auth/presentation/AuthController.java
@@ -1,9 +1,13 @@
 package com.example.moizaspringserver.domain.auth.presentation;
 
+import com.example.moizaspringserver.domain.auth.presentation.dto.request.UserSignInRequest;
 import com.example.moizaspringserver.domain.auth.presentation.dto.response.UserTokenRefreshResponse;
 import com.example.moizaspringserver.domain.auth.service.TokenRefreshService;
+import com.example.moizaspringserver.domain.auth.service.UserSignInService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RequestMapping("/auth")
@@ -11,10 +15,16 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final TokenRefreshService tokenRefreshService;
+    private final UserSignInService userSignInService;
 
-    @PutMapping("/token")
+    @PutMapping("/tokens")
     public UserTokenRefreshResponse userTokenRefresh(@RequestHeader("Refresh-Token") String refreshToken) {
         return tokenRefreshService.execute(refreshToken);
+    }
+
+    @PostMapping("/tokens")
+    public UserTokenRefreshResponse userSignIn(@RequestBody @Valid UserSignInRequest request) {
+        return userSignInService.execute(request);
     }
 
 }
