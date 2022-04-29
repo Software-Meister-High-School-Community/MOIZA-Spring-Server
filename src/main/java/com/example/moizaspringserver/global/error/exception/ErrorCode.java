d09e523fd4bb2601e9b672e12e46b1d03f6bb1f8
lyutvs <lyutvs@gmail.com>
2022-04-29 11:43:56 +0900

    ⚡️ :: ErrorCode

--- src/main/java/com/example/moizaspringserver/global/error/exception/ErrorCode.java
+++ src/main/java/com/example/moizaspringserver/global/error/exception/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
 
     EXPIRED_TOKEN(401, "Expired Token"),
     INVALID_TOKEN(401, "Invalid Token"),
+    REFRESH_TOKEN_NOT_FOUND(404, "Refresh Token Not Found"),
 
     USER_NOT_FOUND(404, "User Not Found");
 
