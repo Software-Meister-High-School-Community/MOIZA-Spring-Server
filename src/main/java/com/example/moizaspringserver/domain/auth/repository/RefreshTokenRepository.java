33bb2bc62ead376d20e807dc7b9ae10ef347afe5
lyutvs <lyutvs@gmail.com>
2022-04-29 11:43:42 +0900

    ⚡️ :: RefreshTokenRepository

--- src/main/java/com/example/moizaspringserver/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/moizaspringserver/domain/auth/repository/RefreshTokenRepository.java
@@ -3,6 +3,9 @@ package com.example.moizaspringserver.domain.auth.repository;
 import com.example.moizaspringserver.domain.auth.entity.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
