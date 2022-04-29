b3d82adf082293baedb31089e733535cb3ca94f6
lyutvs <lyutvs@gmail.com>
2022-04-29 16:05:28 +0900

    ♻️ :: Response 순서 변경

--- src/main/java/com/example/moizaspringserver/domain/auth/presentation/dto/response/UserTokenRefreshResponse.java
+++ src/main/java/com/example/moizaspringserver/domain/auth/presentation/dto/response/UserTokenRefreshResponse.java
@@ -12,8 +12,9 @@ public class UserTokenRefreshResponse {
 
     private final String accessToken;
 
-    private final String refreshToken;
-
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private final LocalDateTime expiredAt;
+    
+    private final String refreshToken;
+
 }
