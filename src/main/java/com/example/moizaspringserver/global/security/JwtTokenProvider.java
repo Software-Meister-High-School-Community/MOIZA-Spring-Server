b76e9dbb5dcd298b36077162d1346f038f0e343f
lyutvs <lyutvs@gmail.com>
2022-04-29 15:52:30 +0900

    ⚡️ :: 저장로직 추가

--- src/main/java/com/example/moizaspringserver/global/security/JwtTokenProvider.java
+++ src/main/java/com/example/moizaspringserver/global/security/JwtTokenProvider.java
@@ -1,5 +1,7 @@
 package com.example.moizaspringserver.global.security;
 
+import com.example.moizaspringserver.domain.auth.entity.RefreshToken;
+import com.example.moizaspringserver.domain.auth.repository.RefreshTokenRepository;
 import com.example.moizaspringserver.global.error.security.ExpiredTokenException;
 import com.example.moizaspringserver.global.error.security.InvalidTokenException;
 import com.example.moizaspringserver.global.security.auth.AuthDetailsService;
@@ -21,15 +23,24 @@ import java.util.Date;
 @RequiredArgsConstructor
 public class JwtTokenProvider {
 
+    public static final String ACCESS_KEY = "access";
+    public static final String REFRESH_KEY = "refresh";
+
     private final JwtProperties jwtProperties;
     private final AuthDetailsService authDetailsService;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     public String generateAccessToken(String id) {
-        return generateToken(id, "access", jwtProperties.getAccessExp());
+        return generateToken(id, ACCESS_KEY, jwtProperties.getAccessExp());
     }
 
     public String generateRefreshToken(String id) {
-        return generateToken(id, "refresh", jwtProperties.getRefreshExp());
+        String refreshToken = generateToken(id, REFRESH_KEY, jwtProperties.getRefreshExp());
+        refreshTokenRepository.save(RefreshToken.builder()
+                .accountId(id)
+                .refreshToken(refreshToken)
+                .build());
+        return refreshToken;
     }
 
     public String resolveToken(HttpServletRequest request) {
