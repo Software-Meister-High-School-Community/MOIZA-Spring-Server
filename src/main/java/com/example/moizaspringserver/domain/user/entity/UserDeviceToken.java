8ce20d57335b18170dc7336807b2c3902f0176aa
lyutvs <lyutvs@gmail.com>
2022-05-03 15:40:12 +0900

    📑 :: UserDeviceToken

---
+++ src/main/java/com/example/moizaspringserver/domain/user/entity/UserDeviceToken.java
@@ -0,0 +1,42 @@
+package com.example.moizaspringserver.domain.user.entity;
+
+import com.example.moizaspringserver.global.entity.BaseIdEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "tbl_user_device_token")
+public class UserDeviceToken extends BaseIdEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @NotNull
+    @Length(max = 64)
+    private String appDeviceToken;
+
+    @NotNull
+    @Length(max = 64)
+    private String webDeviceToken;
+
+    @Builder
+    public UserDeviceToken(User user, String appDeviceToken, String webDeviceToken) {
+        this.user = user;
+        this.appDeviceToken = appDeviceToken;
+        this.webDeviceToken = webDeviceToken;
+    }
+
+    public void setDeviceToken(String appDeviceToken, String webDeviceToken) {
+        this.appDeviceToken = appDeviceToken;
+        this.webDeviceToken = webDeviceToken;
+    }
+}
