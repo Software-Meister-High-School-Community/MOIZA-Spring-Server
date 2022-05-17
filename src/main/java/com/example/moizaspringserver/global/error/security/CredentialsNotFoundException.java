8ee23cc833932cfc0aed8eea2f02b948bc8ac4f0
KimBeomJin <sdpthf@naver.com>
2022-05-17 20:18:31 +0900

    📑 :: CredentialsNotFoundException

---
+++ src/main/java/com/example/moizaspringserver/global/error/security/CredentialsNotFoundException.java
@@ -0,0 +1,13 @@
+package com.example.moizaspringserver.global.error.security;
+
+import com.example.moizaspringserver.global.error.exception.CustomException;
+import com.example.moizaspringserver.global.error.exception.ErrorCode;
+
+public class CredentialsNotFoundException extends CustomException {
+
+    public static final CustomException EXCEPTION = new CredentialsNotFoundException();
+
+    private CredentialsNotFoundException() {
+        super(ErrorCode.CREDENTIALS_NOT_FOUND);
+    }
+}
