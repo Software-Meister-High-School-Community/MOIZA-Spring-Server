e350b967dfb53eec26978d54a2518b4cd956121a
이준서 <ljchan71@gmail.com>
2022-05-17 20:10:57 +0900

    📑 :: EmailCodeLimit 생성

---
+++ src/main/java/com/example/moizaspringserver/domain/auth/entity/EmailCodeLimit.java
@@ -0,0 +1,37 @@
+package com.example.moizaspringserver.domain.auth.entity;
+
+import com.example.moizaspringserver.domain.auth.type.Type;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RedisHash
+public class EmailCodeLimit {
+
+	@Id
+	private String email;
+
+	private Type type;
+
+	private long timeToLive;
+
+	private int count;
+
+	@Builder
+	public EmailCodeLimit(String email, Type type) {
+		this.email = email;
+		this.type = type;
+		this.timeToLive = 3600 * 24 * 1000L;
+		this.count = 1;
+	}
+
+	public EmailCodeLimit addCount() {
+		this.count ++;
+		return this;
+	}
+}
