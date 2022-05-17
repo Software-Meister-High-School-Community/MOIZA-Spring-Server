0418637429dd181151c74ed9308b09f3c9af1131
VINTO1819 <develretr0@gmail.com>
2022-05-17 12:02:51 +0900

    📑 :: FollowService 및 DTO

---
+++ src/main/java/com/example/moizaspringserver/domain/follow/service/FollowService.java
@@ -0,0 +1,41 @@
+package com.example.moizaspringserver.domain.follow.service;
+
+import com.example.moizaspringserver.domain.follow.entity.Follow;
+import com.example.moizaspringserver.domain.follow.presentation.dto.response.FollowingInfo;
+import com.example.moizaspringserver.domain.follow.presentation.dto.response.GetAllFollowingResponse;
+import com.example.moizaspringserver.domain.follow.repository.FollowRepository;
+import com.example.moizaspringserver.domain.user.entity.User;
+import com.example.moizaspringserver.domain.user.exception.UserNotFoundException;
+import com.example.moizaspringserver.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class FollowService {
+    private final UserRepository userRepository;
+    private final FollowRepository followRepository;
+
+    public GetAllFollowingResponse getAllFollowing(Integer userId) {
+        final Optional<User> user = userRepository.findById(userId);
+        if(user.isEmpty()) throw UserNotFoundException.EXCEPTION;
+
+        final List<Follow> followings = followRepository.findAllByUser(user.get());
+        List<FollowingInfo> followingList = followings.stream().map((follow) -> {
+            User targetUser = follow.getTargetUser();
+            return FollowingInfo.builder()
+                    .userId(targetUser.getId())
+                    .name(targetUser.getName())
+                    .profileImageUrl(targetUser.getProfileImageUrl())
+                    .userScope(targetUser.getUserType())
+                    .build();
+        }).collect(Collectors.toList());
+
+        return new GetAllFollowingResponse(followingList);
+    }
+
+}
