e6fbfb23e95ac33733fa43109fce877b49bc5c54
VINTO1819 <develretr0@gmail.com>
2022-05-17 12:05:23 +0900

    ⚡️:: FollowRepository 추상메서드 추가

--- src/main/java/com/example/moizaspringserver/domain/follow/repository/FollowRepository.java
+++ src/main/java/com/example/moizaspringserver/domain/follow/repository/FollowRepository.java
@@ -2,7 +2,13 @@ package com.example.moizaspringserver.domain.follow.repository;
 
 import com.example.moizaspringserver.domain.follow.entity.Follow;
 import com.example.moizaspringserver.domain.follow.entity.FollowId;
+import com.example.moizaspringserver.domain.user.entity.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface FollowRepository extends CrudRepository<Follow, FollowId> {
+    public List<Follow> findAllByUser(User user);
 }
