7576bd54ff130ce9d3dacd215df657f539c38604
이준서 <ljchan71@gmail.com>
2022-05-19 10:31:44 +0900

    ⚡️ :: 요청사항

--- src/main/java/com/example/moizaspringserver/global/aws/properties/AwsProperties.java
+++ src/main/java/com/example/moizaspringserver/global/aws/properties/AwsProperties.java
@@ -1,10 +1,12 @@
 package com.example.moizaspringserver.global.aws.properties;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
 @Getter
+@AllArgsConstructor
 @ConstructorBinding
 @ConfigurationProperties(prefix = "aws")
 public class AwsProperties {
@@ -12,10 +14,4 @@ public class AwsProperties {
 	private final String accessKey;
 	private final String secretKey;
 	private final String sesSource;
-
-	public AwsProperties(String accessKey, String secretKey, String sesSource) {
-		this.accessKey = accessKey;
-		this.secretKey = secretKey;
-		this.sesSource = sesSource;
-	}
 }
