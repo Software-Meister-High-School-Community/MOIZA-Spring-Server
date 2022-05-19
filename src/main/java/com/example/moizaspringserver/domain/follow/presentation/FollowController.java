3f51dec763b2de1a33e856eca83bcf722c396749
VINTO1819 <develretr0@gmail.com>
2022-05-19 20:20:57 +0900

    ⚡️:: 팔로우 생성 API

--- src/main/java/com/example/moizaspringserver/domain/follow/presentation/FollowController.java
+++ src/main/java/com/example/moizaspringserver/domain/follow/presentation/FollowController.java
@@ -1,21 +1,27 @@
 package com.example.moizaspringserver.domain.follow.presentation;
 
 import com.example.moizaspringserver.domain.follow.presentation.dto.response.GetAllFollowingResponse;
-import com.example.moizaspringserver.domain.follow.service.FollowService;
+import com.example.moizaspringserver.domain.follow.service.FollowEstablishService;
+import com.example.moizaspringserver.domain.follow.service.FollowInquireService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/follow")
 public class FollowController {
-    private final FollowService followService;
+    private final FollowInquireService followInquireService;
+    private final FollowEstablishService followEstablishService;
+
+    @PostMapping("/{user-id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void newFollow(@PathVariable("user-id") Integer userId) {
+        followEstablishService.newFollow(userId);
+    }
 
     @GetMapping("/following/{user-id}")
     public GetAllFollowingResponse getAllFollowing(@PathVariable("user-id") Integer userId) {
-        return followService.getAllFollowing(userId);
+        return followInquireService.getAllFollowing(userId);
     }
 }
