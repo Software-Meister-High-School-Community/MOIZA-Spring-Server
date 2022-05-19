0d07b7c2ff9287993cbc0265e2ee9ae23e6168df
VINTO1819 <develretr0@gmail.com>
2022-05-19 11:18:36 +0900

    ♻️:: 미사용 메서드 삭제 및 누락된 쿼리 추가

--- src/main/java/com/example/moizaspringserver/domain/follow/repository/FollowRepository.java
+++ src/main/java/com/example/moizaspringserver/domain/follow/repository/FollowRepository.java
@@ -5,14 +5,13 @@ import com.example.moizaspringserver.domain.follow.entity.FollowId;
 import com.example.moizaspringserver.domain.user.entity.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface FollowRepository extends CrudRepository<Follow, FollowId> {
-    @Query("select follow from Follow follow join fetch follow.targetUser")
-    List<Follow> findAllByUserWithTarget(User user);
-
-    List<Follow> findAllByTargetUser(User targetUser);
+    @Query("select follow from Follow follow join fetch follow.targetUser where follow.user=:user")
+    List<Follow> findAllByUserWithTarget(@Param("user") User user);
 }
