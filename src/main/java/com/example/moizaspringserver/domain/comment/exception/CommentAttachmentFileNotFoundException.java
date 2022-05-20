af71467ceafed71565ab689e19be82c715c1c3d2
lyutvs <lyutvs@gmail.com>
2022-05-20 23:47:39 +0900

    📑 :: CommentAttachmentFIleNotFoundExpception

---
+++ src/main/java/com/example/moizaspringserver/domain/comment/exception/CommentAttachmentFileNotFoundException.java
@@ -0,0 +1,14 @@
+package com.example.moizaspringserver.domain.comment.exception;
+
+import com.example.moizaspringserver.global.error.exception.CustomException;
+import com.example.moizaspringserver.global.error.exception.ErrorCode;
+
+public class CommentAttachmentFileNotFoundException extends CustomException {
+
+    public static final CustomException EXCEPTION =
+            new CommentAttachmentFileNotFoundException();
+
+    private CommentAttachmentFileNotFoundException() {
+        super(ErrorCode.COMMNET_ATTACHMENT_FILE_NOT_FOUND);
+    }
+}
