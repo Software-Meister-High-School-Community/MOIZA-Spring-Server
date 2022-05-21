0ca98cdffbfff728b8fc7f0dee1df17935b71791
lyutvs <lyutvs@gmail.com>
2022-05-21 20:01:55 +0900

    📑 :: NoticeController

---
+++ src/main/java/com/example/moizaspringserver/domain/notice/presentation/NoticeController.java
@@ -0,0 +1,9 @@
+package com.example.moizaspringserver.domain.notice.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@RequestMapping("/notices")
+public class NoticeController {
+}
