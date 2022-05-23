45a8a6929fbbd26475b8380cadccf0f8c462dba2
VINTO1819 <develretr0@gmail.com>
2022-05-23 10:32:25 +0900

    ♻️:: DTO 공통부분에서 FollowingInfo 재작성

--- src/main/java/com/example/moizaspringserver/domain/follow/presentation/dto/response/FollowingInfo.java
+++ src/main/java/com/example/moizaspringserver/domain/follow/presentation/dto/response/FollowingInfo.java
@@ -3,18 +3,10 @@ package com.example.moizaspringserver.domain.follow.presentation.dto.response;
 import com.example.moizaspringserver.domain.user.type.School;
 import com.example.moizaspringserver.global.enums.UserType;
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
-@Builder
-public class FollowingInfo {
-    private final Integer userId;
-
-    private final String name;
-
-    private final String profileImageUrl;
-
-    private final UserType userScope;
-
-    private final School school;
+public class FollowingInfo extends CommonFollowInfo {
+    @Builder
+    FollowingInfo(Integer userId, String name, String profileImageUrl, UserType userScope, School school) {
+        super(userId, name, profileImageUrl, userScope, school);
+    }
 }
