614e1b8d55d544f5b109638356b72c49e40ebb7f
VINTO1819 <develretr0@gmail.com>
2022-05-23 10:51:18 +0900

    ♻️:: 누락된 Getter 추가

--- src/main/java/com/example/moizaspringserver/domain/follow/presentation/dto/response/FollowerInfo.java
+++ src/main/java/com/example/moizaspringserver/domain/follow/presentation/dto/response/FollowerInfo.java
@@ -3,7 +3,9 @@ package com.example.moizaspringserver.domain.follow.presentation.dto.response;
 import com.example.moizaspringserver.domain.user.type.School;
 import com.example.moizaspringserver.global.enums.UserType;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 public class FollowerInfo extends CommonFollowInfo {
     private final boolean isFollow;
 
