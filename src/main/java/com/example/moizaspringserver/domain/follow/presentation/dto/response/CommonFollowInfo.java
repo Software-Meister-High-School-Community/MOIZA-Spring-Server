6168d16e2a904e6265ce24894417eded16460ba3
VINTO1819 <develretr0@gmail.com>
2022-05-23 10:31:00 +0900

    ♻️:: DTO 공통부분 분리

---
+++ src/main/java/com/example/moizaspringserver/domain/follow/presentation/dto/response/CommonFollowInfo.java
@@ -0,0 +1,21 @@
+package com.example.moizaspringserver.domain.follow.presentation.dto.response;
+
+import com.example.moizaspringserver.domain.user.type.School;
+import com.example.moizaspringserver.global.enums.UserType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CommonFollowInfo {
+    private final Integer userId;
+
+    private final String name;
+
+    private final String profileImageUrl;
+
+    private final UserType userScope;
+
+    private final School school;
+}
