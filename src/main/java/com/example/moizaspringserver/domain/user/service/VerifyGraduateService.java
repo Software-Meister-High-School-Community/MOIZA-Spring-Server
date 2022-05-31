812308edd14a1763673949c6ae055d3d9addca9b
KimBeomJin <sdpthf@naver.com>
2022-06-01 02:30:50 +0900

    📑 :: VerifyGraduateService

---
+++ src/main/java/com/example/moizaspringserver/domain/user/service/VerifyGraduateService.java
@@ -0,0 +1,32 @@
+package com.example.moizaspringserver.domain.user.service;
+
+import com.example.moizaspringserver.domain.user.entity.GraduateVerifyingFile;
+import com.example.moizaspringserver.domain.user.entity.User;
+import com.example.moizaspringserver.domain.user.facade.UserFacade;
+import com.example.moizaspringserver.domain.user.presentation.dto.request.VerifyGraduateRequest;
+import com.example.moizaspringserver.domain.user.repository.GraduateVerifyingFileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class VerifyGraduateService {
+
+    private final UserFacade userFacade;
+    private final GraduateVerifyingFileRepository graduateVerifyingFileRepository;
+
+    @Transactional
+    public void execute(VerifyGraduateRequest request) {
+        String verifyingFileUrl = request.getVerifyingFileUrl();
+        User user = userFacade.queryCurrentUser();
+
+        GraduateVerifyingFile verifyingFile = GraduateVerifyingFile.builder()
+                .user(user)
+                .verifyingFileUrl(verifyingFileUrl)
+                .build();
+
+        graduateVerifyingFileRepository.save(verifyingFile);
+    }
+
+}
