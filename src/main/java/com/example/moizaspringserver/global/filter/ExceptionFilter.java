6dad5aa79c2296d8aa928a4921d8223164f4955c
lyutvs <lyutvs@gmail.com>
2022-06-16 01:11:26 +0900

    📑 :: ExpceptionFilter

---
+++ src/main/java/com/example/moizaspringserver/global/filter/ExceptionFilter.java
@@ -0,0 +1,46 @@
+package com.example.moizaspringserver.global.filter;
+
+import com.example.moizaspringserver.global.error.ErrorResponse;
+import com.example.moizaspringserver.global.error.exception.CustomException;
+import com.example.moizaspringserver.global.error.exception.ErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class ExceptionFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (CustomException e) {
+            sendErrorMessage(response, e.getErrorCode());
+        } catch (Exception e) {
+            logger.error(e);
+            sendErrorMessage(response, ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private void sendErrorMessage(HttpServletResponse response, ErrorCode errorCode) throws IOException {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .status(errorCode.getStatus())
+                .message(errorCode.getMessage())
+                .build();
+        String errorResponseJson = objectMapper.writeValueAsString(errorResponse);
+
+        response.setStatus(errorCode.getStatus());
+        response.setContentType("application/json");
+        response.getWriter().write(errorResponseJson);
+    }
+}
