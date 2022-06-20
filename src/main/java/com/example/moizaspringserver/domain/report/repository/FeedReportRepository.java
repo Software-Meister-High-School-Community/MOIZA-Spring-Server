1a197b074ed8c2c935cd335efeb765a445a10a6e
KimBeomJin <sdpthf@naver.com>
2022-06-20 18:51:19 +0900

    ⚡️ :: 게시글과 사용자로 신고 조회

--- src/main/java/com/example/moizaspringserver/domain/report/repository/FeedReportRepository.java
+++ src/main/java/com/example/moizaspringserver/domain/report/repository/FeedReportRepository.java
@@ -3,10 +3,16 @@ package com.example.moizaspringserver.domain.report.repository;
 import com.example.moizaspringserver.domain.feed.entity.Feed;
 import com.example.moizaspringserver.domain.report.entity.FeedReport;
 import com.example.moizaspringserver.domain.report.entity.FeedReportId;
+import com.example.moizaspringserver.domain.report.entity.Report;
+import com.example.moizaspringserver.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface FeedReportRepository extends JpaRepository<FeedReport, FeedReportId> {
 
     void deleteByFeed(Feed feed);
+
+    Optional<FeedReport> findByFeedAndReportUser(Feed feed, User user);
 
 }
