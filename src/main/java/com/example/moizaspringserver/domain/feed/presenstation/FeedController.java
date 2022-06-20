98eb148931b3d2921e4264fb29179ee1435c0b57
KimBeomJin <sdpthf@naver.com>
2022-06-20 18:51:43 +0900

    ⚡️ :: 게시글 신고 컨트롤러

--- src/main/java/com/example/moizaspringserver/domain/feed/presenstation/FeedController.java
+++ src/main/java/com/example/moizaspringserver/domain/feed/presenstation/FeedController.java
@@ -1,6 +1,7 @@
 package com.example.moizaspringserver.domain.feed.presenstation;
 
 import com.example.moizaspringserver.domain.feed.service.DeleteFeedService;
+import com.example.moizaspringserver.domain.feed.service.ReportFeedService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +12,18 @@ import org.springframework.web.bind.annotation.*;
 public class FeedController {
 
     private final DeleteFeedService deleteFeedService;
+    private final ReportFeedService reportFeedService;
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{feed-id}")
     public void deleteFeed(@PathVariable("feed-id") Integer feedId) {
         deleteFeedService.execute(feedId);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/{feed-id}")
+    public void reportFeed(@PathVariable("feed-id") Integer feedId) {
+        reportFeedService.execute(feedId);
     }
 
 }
