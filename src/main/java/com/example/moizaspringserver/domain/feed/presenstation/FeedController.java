4ce469a4645cf47448ef5ebf990368b95617ba1e
KimBeomJin <sdpthf@naver.com>
2022-06-20 17:06:55 +0900

    ⚡️ :: 임시 저장 게시글 삭제 컨트롤러

--- src/main/java/com/example/moizaspringserver/domain/feed/presenstation/FeedController.java
+++ src/main/java/com/example/moizaspringserver/domain/feed/presenstation/FeedController.java
@@ -1,6 +1,7 @@
 package com.example.moizaspringserver.domain.feed.presenstation;
 
 import com.example.moizaspringserver.domain.feed.service.DeleteFeedService;
+import com.example.moizaspringserver.domain.feed.service.DeleteLocalFeedService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +12,18 @@ import org.springframework.web.bind.annotation.*;
 public class FeedController {
 
     private final DeleteFeedService deleteFeedService;
+    private final DeleteLocalFeedService deleteLocalFeedService;
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{feed-id}")
     public void deleteFeed(@PathVariable("feed-id") Integer feedId) {
         deleteFeedService.execute(feedId);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/temporaries/{feed-id}")
+    public void deleteLocalFeed(@PathVariable("feed-id") Integer feedId) {
+        deleteLocalFeedService.execute(feedId);
     }
 
 }
