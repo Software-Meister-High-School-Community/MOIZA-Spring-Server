e285ac5996b1f4cf8f7122b5de3e2cc06a06829a
이준서 <ljchan71@gmail.com>
2022-06-20 20:00:28 +0900

    ⚡️ :: findPopularFeed / PublicFeedRepository

--- src/main/java/com/example/moizaspringserver/domain/feed/respository/PublicFeedRepository.java
+++ src/main/java/com/example/moizaspringserver/domain/feed/respository/PublicFeedRepository.java
@@ -2,10 +2,14 @@ package com.example.moizaspringserver.domain.feed.respository;
 
 import com.example.moizaspringserver.domain.feed.entity.Feed;
 import com.example.moizaspringserver.domain.feed.entity.PublicFeed;
+import java.time.LocalDateTime;
+import java.util.List;
+import javax.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PublicFeedRepository extends JpaRepository<PublicFeed, Integer> {
 
     void deleteByFeed(Feed feed);
+    List<PublicFeed> findTop8ByCreatedAtBetweenOrderByLikeCountDesc(LocalDateTime start, LocalDateTime end);
 
 }
