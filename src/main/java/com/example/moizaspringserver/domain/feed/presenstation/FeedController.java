5328aa24f9670eb23be0d26fcbf70092ff255ee6
이준서 <ljchan71@gmail.com>
2022-06-20 19:07:19 +0900

    ⚡️ :: FeedController

--- src/main/java/com/example/moizaspringserver/domain/feed/presenstation/FeedController.java
+++ src/main/java/com/example/moizaspringserver/domain/feed/presenstation/FeedController.java
@@ -1,6 +1,8 @@
 package com.example.moizaspringserver.domain.feed.presenstation;
 
+import com.example.moizaspringserver.domain.feed.service.AddLikeService;
 import com.example.moizaspringserver.domain.feed.service.DeleteFeedService;
+import javax.persistence.criteria.CriteriaBuilder.In;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +13,18 @@ import org.springframework.web.bind.annotation.*;
 public class FeedController {
 
     private final DeleteFeedService deleteFeedService;
+    private final AddLikeService addLikeService;
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{feed-id}")
     public void deleteFeed(@PathVariable("feed-id") Integer feedId) {
         deleteFeedService.execute(feedId);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PostMapping("/{feed-id}/like")
+    public void like(@PathVariable("feed-id") Integer feedId) {
+        addLikeService.execute(feedId);
     }
 
 }
