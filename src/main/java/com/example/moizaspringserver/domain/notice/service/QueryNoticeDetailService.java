ef19a29b017ffd884ade37ff93f836fa46b4bf40
lyutvs <lyutvs@gmail.com>
2022-06-21 22:39:08 +0900

    ⚡️ :: 임포트 정리

--- src/main/java/com/example/moizaspringserver/domain/notice/service/QueryNoticeDetailService.java
+++ src/main/java/com/example/moizaspringserver/domain/notice/service/QueryNoticeDetailService.java
@@ -11,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.validation.constraints.Null;
 import java.util.List;
 import java.util.stream.Collectors;
 
