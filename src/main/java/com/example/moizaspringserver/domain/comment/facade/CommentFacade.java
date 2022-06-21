93035096f1827964762c3e0cf6c06dadad6613ac
lyutvs <lyutvs@gmail.com>
2022-06-21 22:37:15 +0900

    ⚡️:: 내꺼 인지 검증

--- src/main/java/com/example/moizaspringserver/domain/comment/facade/CommentFacade.java
+++ src/main/java/com/example/moizaspringserver/domain/comment/facade/CommentFacade.java
@@ -5,6 +5,8 @@ import com.example.moizaspringserver.domain.comment.entity.Comment;
 import com.example.moizaspringserver.domain.comment.exception.CommentNotFoundException;
 import com.example.moizaspringserver.domain.comment.repository.CommentRepository;
 import com.example.moizaspringserver.domain.feed.entity.Feed;
+import com.example.moizaspringserver.domain.user.entity.User;
+import com.example.moizaspringserver.global.error.security.InvalidRoleException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +19,19 @@ public class CommentFacade {
     public Comment getAllByFeed(Feed feed) {
         return commentRepository.findAllByFeed(feed)
                 .orElseThrow(() -> CommentNotFoundException.EXCEPTION);
+    }
+
+    public Comment getCommentById(Integer id) {
+        return commentRepository.findById(id)
+                .orElseThrow(() -> CommentNotFoundException.EXCEPTION);
+    }
+
+    public Boolean getMatchMyComment(Integer id, User user) {
+        if (!user.equals(getCommentById(id).getUser())) {
+            throw InvalidRoleException.EXCEPTION;
+        } else {
+            return true;
+        }
     }
 
 }
