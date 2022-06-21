8daa332bf12f48b80374ce2680b4341dd7048df2
lyutvs <lyutvs@gmail.com>
2022-06-21 22:36:56 +0900

    ⚡️ :: 카테고리 조회

--- src/main/java/com/example/moizaspringserver/domain/category/repository/FeedCategoryRepository.java
+++ src/main/java/com/example/moizaspringserver/domain/category/repository/FeedCategoryRepository.java
@@ -5,8 +5,12 @@ import com.example.moizaspringserver.domain.category.entity.FeedCategoryId;
 import com.example.moizaspringserver.domain.feed.entity.Feed;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface FeedCategoryRepository extends CrudRepository<FeedCategory, FeedCategoryId> {
 
     void deleteByFeed(Feed feed);
+
+    Optional<FeedCategory> findByFeed(Feed feed);
 
 }
