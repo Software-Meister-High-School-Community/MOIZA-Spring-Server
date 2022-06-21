a4e196626046a3309637f298278bf5982ff539f0
lyutvs <lyutvs@gmail.com>
2022-06-21 22:39:39 +0900

    ⚡️ :: 피드 조회

--- src/main/java/com/example/moizaspringserver/domain/feed/respository/PublicFeedRepository.java
+++ src/main/java/com/example/moizaspringserver/domain/feed/respository/PublicFeedRepository.java
@@ -3,9 +3,14 @@ package com.example.moizaspringserver.domain.feed.respository;
 import com.example.moizaspringserver.domain.feed.entity.Feed;
 import com.example.moizaspringserver.domain.feed.entity.PublicFeed;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.parameters.P;
+
+import java.util.Optional;
 
 public interface PublicFeedRepository extends JpaRepository<PublicFeed, Integer> {
 
     void deleteByFeed(Feed feed);
+
+    Optional<PublicFeed> findByFeed(Feed feed);
 
 }
