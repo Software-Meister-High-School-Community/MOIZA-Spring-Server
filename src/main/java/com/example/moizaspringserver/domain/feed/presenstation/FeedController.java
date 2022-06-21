6aeb8ba4ea6e65c1db00f79a6d5f7dfdfdb4f62b
lyutvs <lyutvs@gmail.com>
2022-06-22 00:26:30 +0900

    ⚡️ :: FeedController

--- src/main/java/com/example/moizaspringserver/domain/feed/presenstation/FeedController.java
+++ src/main/java/com/example/moizaspringserver/domain/feed/presenstation/FeedController.java
@@ -1,9 +1,13 @@
 package com.example.moizaspringserver.domain.feed.presenstation;
 
+import com.example.moizaspringserver.domain.feed.presenstation.dto.request.CreateFeedRequest;
+import com.example.moizaspringserver.domain.feed.service.CreateFeedService;
 import com.example.moizaspringserver.domain.feed.service.DeleteFeedService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RequestMapping("/feeds")
@@ -11,11 +15,18 @@ import org.springframework.web.bind.annotation.*;
 public class FeedController {
 
     private final DeleteFeedService deleteFeedService;
+    private final CreateFeedService createFeedService;
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{feed-id}")
     public void deleteFeed(@PathVariable("feed-id") Integer feedId) {
         deleteFeedService.execute(feedId);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void createFeed(@Valid @RequestBody CreateFeedRequest request) {
+        createFeedService.execute(request);
     }
 
 }
