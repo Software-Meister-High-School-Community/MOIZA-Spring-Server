4bd7bf433ef7c23a642f7d0cdfe8d8e7ba5a914d
lyutvs <lyutvs@gmail.com>
2022-06-22 10:12:01 +0900

    ⚡️ :: Feed 조회

--- src/main/java/com/example/moizaspringserver/domain/feed/facade/FeedFacade.java
+++ src/main/java/com/example/moizaspringserver/domain/feed/facade/FeedFacade.java
@@ -1,8 +1,14 @@
 package com.example.moizaspringserver.domain.feed.facade;
 
 import com.example.moizaspringserver.domain.feed.entity.Feed;
+import com.example.moizaspringserver.domain.feed.entity.FeedAttachmentFile;
+import com.example.moizaspringserver.domain.feed.entity.PublicFeed;
+import com.example.moizaspringserver.domain.feed.exception.FeedAttachmentFileNotFoundException;
 import com.example.moizaspringserver.domain.feed.exception.FeedNotFoundException;
+import com.example.moizaspringserver.domain.feed.exception.PublicFeedNotFoundException;
+import com.example.moizaspringserver.domain.feed.respository.FeedAttachmentFileRepository;
 import com.example.moizaspringserver.domain.feed.respository.FeedRepository;
+import com.example.moizaspringserver.domain.feed.respository.PublicFeedRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,10 +17,21 @@ import org.springframework.stereotype.Component;
 public class FeedFacade {
 
     private final FeedRepository feedRepository;
+    private final PublicFeedRepository publicFeedRepository;
+    private final FeedAttachmentFileRepository feedAttachmentFileRepository;
 
     public Feed getFeedById(Integer id) {
         return feedRepository.findById(id)
                 .orElseThrow(() -> FeedNotFoundException.EXCEPTION);
     }
 
+    public PublicFeed getPublicFeedByFeedId(Feed feed) {
+        return publicFeedRepository.findByFeed(feed)
+                .orElseThrow(() -> PublicFeedNotFoundException.EXCEPTION);
+    }
+
+    public FeedAttachmentFile getFeedAttachmentFileByFeedId(Feed feed) {
+        return feedAttachmentFileRepository.findAllByFeed(feed)
+                .orElseThrow(() -> FeedAttachmentFileNotFoundException.EXCEPTION);
+    }
 }
