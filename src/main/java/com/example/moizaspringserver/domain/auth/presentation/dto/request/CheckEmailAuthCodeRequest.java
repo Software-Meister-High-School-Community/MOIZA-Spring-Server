43703cd3eb4d6ffcb071da8f882cb5d265a7c259
이준서 <ljchan71@gmail.com>
2022-07-04 11:25:41 +0900

    ♻️ :: 리뷰 사항

--- src/main/java/com/example/moizaspringserver/domain/auth/presentation/dto/request/CheckEmailAuthCodeRequest.java
+++ src/main/java/com/example/moizaspringserver/domain/auth/presentation/dto/request/CheckEmailAuthCodeRequest.java
@@ -1,6 +1,8 @@
 package com.example.moizaspringserver.domain.auth.presentation.dto.request;
 
 import com.example.moizaspringserver.domain.auth.type.Type;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,9 +10,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CheckEmailAuthCodeRequest {
 
+	@NotBlank(message = "email은 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
 	private String email;
 
+	@NotBlank(message = "authCode는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
 	private String authCode;
 
+	@NotNull(message = "type은 Null을 허용하지 않습니다.")
 	private Type type;
 }
