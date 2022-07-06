8f1e67264a78a1de5772e1c04dab182d38d79cf9
VINTO1819 <develretr0@gmail.com>
2022-07-06 15:58:30 +0900

    📑 :: 조회를 위한 Repository 메서드 선언

--- src/main/java/com/example/moizaspringserver/domain/feed/respository/FeedRepository.java
+++ src/main/java/com/example/moizaspringserver/domain/feed/respository/FeedRepository.java
@@ -1,7 +1,11 @@
 package com.example.moizaspringserver.domain.feed.respository;
 
 import com.example.moizaspringserver.domain.feed.entity.Feed;
+import com.example.moizaspringserver.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FeedRepository extends JpaRepository<Feed, Integer> {
+    List<Feed> findAllByUser(User user);
 }
